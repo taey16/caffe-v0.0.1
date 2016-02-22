7f4e7f1000ce15ad86dc21705dbc654f0505162f
taey16 <taey1600@gmail.com>
2016-02-22 15:14:14 +0900

    commit

--- python/predict_ilsvrc12_ResNet.py
+++ python/predict_ilsvrc12_ResNet.py
@@ -14,6 +14,10 @@ MODEL_WEIGHT_FILE = '/storage/ImageNet/ILSVRC2012/model/resnet/ResNet_caffe_mode
 MODEL_ORIGINAL_INPUT_SIZE = 256, 256
 MODEL_INPUT_SIZE = 224, 224
 MODEL_MEAN_FILE = '/storage/ImageNet/ILSVRC2012/model/resnet/ResNet_caffe_models/ResNet_mean.binaryproto'
+blob = caffe.proto.caffe_pb2.BlobProto()
+data = open(MODEL_MEAN_FILE, 'rb').read()
+blob.ParseFromString(data)
+MODEL_MEAN_VALUE = np.squeeze(np.array( caffe.io.blobproto_to_array(blob) ))
 
 DATASET_ROOT = '/storage/ImageNet/ILSVRC2012/val/'
 DATASET_INPUT_LIST = '/storage/ImageNet/ILSVRC2012/val_synset.txt'
@@ -22,20 +26,17 @@ FEATURE_JITTER = 10
 
 
 if __name__ == '__main__':
-  import pdb; pdb.set_trace()
-  caffe.set_device(1)
+  #import pdb; pdb.set_trace()
+  caffe.set_device(0)
   caffe.set_mode_gpu()
   net = caffe.Classifier( \
-    MODEL_GOOGLE_DEPLOY_FILE, 
-    MODEL_GOOGLE_WEIGHT_FILE, 
+    model_file=MODEL_DEPLOY_FILE, 
+    pretrained_file=MODEL_WEIGHT_FILE, 
+    image_dims=(MODEL_ORIGINAL_INPUT_SIZE[0], MODEL_ORIGINAL_INPUT_SIZE[1], 3),
+    raw_scale=255., # scale befor mean subtraction
+    input_scale=None, # scale after mean subtraction
     mean = MODEL_MEAN_VALUE, 
     channel_swap = (2, 1, 0) ) 
-
-  layer_name = ['prob']
-
-  input_blobs = net.blobs['data']
-  input_blobs.reshape(\
-    FEATURE_JITTER, 3, MODEL_INPUT_SIZE[0], MODEL_INPUT_SIZE[1])
 
   filenames=['%s/%s' % (DATASET_ROOT, entry.strip().split(' ')[0]) \
     for entry in open('%s' % DATASET_INPUT_LIST)]
@@ -45,37 +46,36 @@ if __name__ == '__main__':
   hit_count, hit5_count, tic_global = 0, 0, datetime.datetime.now()
   for n, fname in enumerate(filenames):
     try:
+      #import pdb; pdb.set_trace()
       tic_load = datetime.datetime.now()
-      im = utils.load_image( fname )
+      im = caffe.io.load_image(fname)
       toc_load = datetime.datetime.now(); elapsed_load = toc_load - tic_load
       tic_resize = datetime.datetime.now()
-      im = im.resize( MODEL_ORIGINAL_INPUT_SIZE, PIL.Image.ANTIALIAS )
+      #im = caffe.io.resize_image(im, \
+      #  (MODEL_ORIGINAL_INPUT_SIZE[0], MODEL_ORIGINAL_INPUT_SIZE[1]))
       toc_resize = datetime.datetime.now(); elapsed_resize = toc_resize - tic_resize
-      im = utils.preprocess(net, im)
-      tic_jittering = datetime.datetime.now()
-    except:
+      tic_predict = datetime.datetime.now()
+      scores = net.predict([im], True) 
+      elapsed = datetime.datetime.now() - tic_predict
+    except Exception as err:
       print 'error: filename: ', fname
-    if FEATURE_JITTER == 10:
-      im_jittered = utils.oversample(im, MODEL_INPUT_SIZE)
-      input_blobs.data[:] = im_jittered
-    else: input_blobs.data[:] = im[:]
-    toc_jittering = datetime.datetime.now(); 
-    elapsed_jittering = toc_jittering - tic_jittering
+      print err
 
-    tic = datetime.datetime.now()
-    net.forward(end=layer_name[0])
-    toc = datetime.datetime.now(); elapsed = toc-tic
-    dst = net.blobs[layer_name[0]].data
-
-    score = np.mean(dst, axis=0)
+    score = np.mean(scores, axis=0)
     lab_pred=np.argsort(score)[::-1]
 
     if lab_pred[0] == int(labels[n]): hit_count += 1;
     if (lab_pred[0:5] == int(labels[n])).sum() == 1: hit5_count += 1
 
-    if (n+1) % 100 == 0: 
-      print 'acc@1: %f(%d/%d) acc@5: %f(%d/%d) in pred:%02.4f, load:%02.4f, resize:%02.4f, jitter:%02.4f msec.' % \
-        ((hit_count/(n+1.0))*100.0, hit_count, n+1, (hit5_count/(n+1.0))*100, hit5_count, n+1.0, elapsed.microseconds/1000, elapsed_load.microseconds/1000, elapsed_resize.microseconds/1000, elapsed_jittering.microseconds/1000); sys.stdout.flush()
+    if (n+1) % 1 == 0: 
+      print 'acc@1: %f(%d/%d) acc@5: %f(%d/%d) in pred:%02.4f, load:%02.4f, resize:%02.4f msec.' % \
+        ((hit_count/(n+1.0))*100.0, 
+         hit_count, n+1, 
+        (hit5_count/(n+1.0))*100, 
+         hit5_count, n+1.0, 
+        elapsed.microseconds/1000, 
+        elapsed_load.microseconds/1000, 
+        elapsed_resize.microseconds/1000); sys.stdout.flush()
 
 toc_global = datetime.datetime.now()
 elapsed = toc_global - tic_global
